64920e5d1e595943c9851264af42f213bf99a8ef
null6666 <1123958513@QQ.com>
2023-03-29 09:35:58 +0800

    代码提交信息

--- miniprogram-3/pages/login/login.js
+++ miniprogram-3/pages/login/login.js
@@ -1,11 +1,10 @@
 const app = getApp()
-
 const defaultAvatarUrl = 'https://mmbiz.qpic.cn/mmbiz/icTdbqWNOwNRna42FI242Lcia07jQodd2FJGIYQfG0LAJGFxM4FbnQP6yfMxBgJ0F3YRqJCJ1aPAK2dQagdusBZg/0'
-
-
 Page({
   data: {
     avatarUrl: defaultAvatarUrl,
+    getImg : false,
+    getName : false
   },
   onLoad() {
   },
@@ -13,6 +12,14 @@ Page({
     const { avatarUrl } = e.detail 
     this.setData({
       avatarUrl,
+      getImg : true
     })
+  },
+  jump(){
+    wx.switchTab({url:'/pages/index/index'})
+  },
+  switch(){this.setData({
+    getName : true
+  })
   }
 })
